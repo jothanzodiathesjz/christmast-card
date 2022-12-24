189932663c5d3650ec53069ebb8cd6f8216130bf
Jothan Zodiathes <jothanzodiathes@gmail.com>
2022-12-25 03:02:36 +0800

    change json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 import './assets/css/main.scss'
-// import { useCallback } from "react";
-// import Particles from "react-particles";
-// import { loadFull } from "https://cdn.jsdelivr.net/npm/tsparticles/+esm"
-// import Hero from './components/Hero';
-// import Headers from './components/Headers';
-// import { Config } from './components/Config';
-
-import ModalFamily from './components/ModalFamily';
 import Paralax from './components/Paralax';
 
 function App() {
